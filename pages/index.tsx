09fcb07ce8b297c6bf931b6defbc6c9d0d58cb19
Mateusz Brzeziński <mateusz0brzezinski@gmail.com>
2024-04-07 23:16:29 +0200

    chore: update OG photo

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,8 +14,8 @@ export default function home() {
 
         <meta property="og:title" content="Mateusz Brzeziński | Frontend Developer" />
         <meta property="og:type" content="article" />
-        <meta property="og:url" content="https://deverajc.com/" />
-        <meta property="og:image" content="https://deverajc.com/screenshot.png" />
+        <meta property="og:url" content="https://mateusz-brzezinski.pl/" />
+        <meta property="og:image" content="https://mateusz-brzezinski.pl/screenshot.png" />
         <meta property="og:description" content="Hey! I'm Mateusz Brzeziński, a Frontend Developer based in Szczecin, Poland." />
 
         <meta name="twitter:card" content="summary"/>
@@ -23,7 +23,7 @@ export default function home() {
         <meta name="twitter:title" content="Mateusz Brzeziński | Frontend Developer"/>
         <meta name="twitter:description" content="Hey! I'm Mateusz Brzeziński, a Frontend Developer based in Szczecin, Poland."/>
         <meta name="twitter:creator" content="@jc_devz"/>
-        <meta name="twitter:image" content="https://deverajc.com/screenshot.png"/>
+        <meta name="twitter:image" content="https://mateusz-brzezinski.pl//screenshot.png"/>
         <meta name="google-site-verification" content="UQ5sUXYifK60YioNVOq0GqH13Gl3JvOZBZ_9YkvAp4A" />
       </Head>
       <Home />
