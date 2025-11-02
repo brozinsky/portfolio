96ce213b18069ebebc6d14fe6b2b9a6358405c8b
Mateusz Brzeziński <mateusz0brzezinski@gmail.com>
2025-11-02 22:49:17 +0100

    chore: add GA

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import type { AppProps } from "next/app";
 import { Poppins } from "next/font/google";
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import Script from "next/script";
 
 const poppins = Poppins({
   weight: ["100", "200", "400", "700", "900"],
@@ -11,10 +12,28 @@ const poppins = Poppins({
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <div id="root" className={poppins.className}>
-      <Component {...pageProps} />
-      <Analytics />
-      <SpeedInsights />
-    </div>
+    <>
+      <Script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-VZ56HDKR1J"
+      />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-VZ56HDKR1J');
+          `,
+        }}
+      />
+      <div id="root" className={poppins.className}>
+        <Component {...pageProps} />
+        <Analytics />
+        <SpeedInsights />
+      </div>
+    </>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,23 +6,47 @@ export default function HomePage() {
     <>
       <Head>
         <title>Mateusz Brzeziński | Frontend Developer</title>
-        <meta name="description" content="Mateusz Brzeziński – Frontend Developer in Szczecin | React, TypeScript, Next.js Specialist" />
+        <meta
+          name="description"
+          content="Mateusz Brzeziński – Frontend Developer in Szczecin | React, TypeScript, Next.js Specialist"
+        />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
 
-        <meta property="og:title" content="Mateusz Brzeziński | Frontend Developer" />
+        <meta
+          property="og:title"
+          content="Mateusz Brzeziński | Frontend Developer"
+        />
         <meta property="og:type" content="website" />
         <meta property="og:url" content="https://mateusz-brzezinski.pl/" />
-        <meta property="og:image" content="https://mateusz-brzezinski.pl/screenshot.png" />
-        <meta property="og:description" content="Hey! I'm Mateusz Brzeziński, a Frontend Developer based in Szczecin, Poland." />
+        <meta
+          property="og:image"
+          content="https://mateusz-brzezinski.pl/screenshot.png"
+        />
+        <meta
+          property="og:description"
+          content="Hey! I'm Mateusz Brzeziński, a Frontend Developer based in Szczecin, Poland."
+        />
 
-        <meta name="twitter:card" content="summary_large_image"/>
-        <meta name="twitter:site" content="@jc_devz"/>
-        <meta name="twitter:title" content="Mateusz Brzeziński | Frontend Developer"/>
-        <meta name="twitter:description" content="Hey! I'm Mateusz Brzeziński, a Frontend Developer based in Szczecin, Poland."/>
-        <meta name="twitter:creator" content="@jc_devz"/>
-        <meta name="twitter:image" content="https://mateusz-brzezinski.pl/screenshot.png"/>
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@jc_devz" />
+        <meta
+          name="twitter:title"
+          content="Mateusz Brzeziński | Frontend Developer"
+        />
+        <meta
+          name="twitter:description"
+          content="Hey! I'm Mateusz Brzeziński, a Frontend Developer based in Szczecin, Poland."
+        />
+        <meta name="twitter:creator" content="@jc_devz" />
+        <meta
+          name="twitter:image"
+          content="https://mateusz-brzezinski.pl/screenshot.png"
+        />
 
-        <meta name="google-site-verification" content="UQ5sUXYifK60YioNVOq0GqH13Gl3JvOZBZ_9YkvAp4A" />
+        <meta
+          name="google-site-verification"
+          content="UQ5sUXYifK60YioNVOq0GqH13Gl3JvOZBZ_9YkvAp4A"
+        />
 
         <link rel="shortcut icon" href="/favicon.png" />
       </Head>
